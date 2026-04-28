b6a47da5a62132389e5fb584d81707277e22ec03
Milkentiy <128047261+ilovepiton@users.noreply.github.com>
2026-04-28 10:06:31 +0200

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,1 @@
 console.log("Mogamed Office is running");
-
-const isLoggedIn = localStorage.getItem("mogamedLoggedIn");
-const currentPath = window.location.pathname;
-const isLoginPage = currentPath.includes("login.html");
-
-if (isLoggedIn !== "yes" && !isLoginPage) {
-  if (currentPath.includes("/pages/")) {
-    window.location.href = "login.html";
-  } else {
-    window.location.href = "pages/login.html";
-  }
-}
-
-function logout() {
-  localStorage.removeItem("mogamedLoggedIn");
-  window.location.href = "pages/login.html";
-}
